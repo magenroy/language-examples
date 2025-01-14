95947917ad0eb5c8ac99293fc76f63a8e9d0bd24
Roy Magen <magenroy@gmail.com>
2025-01-14 16:16:10 -0500

    stuff

--- rust_examples/src/patterns.rs
+++ rust_examples/src/patterns.rs
@@ -3,6 +3,7 @@ struct Simple<T>(T);
 fn a<T>(Simple(v) : Simple<T>) -> T { v }
 
 // REF: https://rust-lang.github.io/rfcs/2005-match-ergonomics.html
+// REF: https://doc.rust-lang.org/reference/patterns.html#reference-patterns
 fn pattern_reference<T>(x: &Simple<T>) -> &T {
     let Simple(v) = x;
     v
