f4c07fc3a57cb249ea2fb47981949749202877d2
Roy Magen <magenroy@gmail.com>
2025-01-17 12:09:57 -0500

    stuff

---
+++ rust_examples/src/control_flow.rs
@@ -0,0 +1,13 @@
+// REF: https://cheats.rs/#control-flow
+
+// TODO:
+// early `return`
+//  also inside of blocks and closures
+// `while`
+// `loop`
+// `for`
+// block labels and loop labels
+//  `break` and `continue`, possibly with labels
+// `?`
+// `await`
+// `async`?
--- rust_examples/src/lib.rs
+++ rust_examples/src/lib.rs
@@ -5,3 +5,4 @@ mod ownership;
 mod mutation;
 mod patterns;
 mod implement;
+mod control_flow;
